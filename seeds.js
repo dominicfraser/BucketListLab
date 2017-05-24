216f8fd03167c0d924ee39dc32fff1ac8dd9e31a
Reece Baird <reeceybaird@hotmail.com>
2017-05-24 10:50:52 +0100

    seeds file added

---
+++ seeds.js
@@ -0,0 +1,15 @@
+use world_countries;
+
+db.countries.insert ([
+  {
+    name: "Country 1",
+    population: "100",
+    capital: "Capital 1"
+  },
+
+  {
+    name: "Country 2",
+    population: "200",
+    capital: "Capital 2"
+  },
+])
