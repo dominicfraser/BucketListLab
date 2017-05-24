14c8351fdd8b1536fd9856c419e5a3005cb3d23b
Reece Baird <reeceybaird@hotmail.com>
2017-05-24 11:18:55 +0100

    db Query Helper created but not tested

---
+++ db/dbQueryHelper.js
@@ -0,0 +1,33 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var dbQueryHelper = function(){
+  this.url = "mongodb://localhost:27017/world_countries"
+};
+
+
+dbQueryHelper.prototype = {
+  add: function(countryToAdd, onQueryFinishedCallback){
+    MongoClient.connect(this.url, function(err, db){
+      if (db){
+        var collection = db.collection('countries');
+        collection.insert(countryToAdd);
+        collection.find().toArray(function(err, docs){
+          onQueryFinishedCallback(docs);
+        })
+      }
+    })
+  },
+
+  all: function(onQueryFinishedCallback){
+    MongoClient.connect(this.url, function(err, db){
+      if (db){
+        var collection = db.collection('countries');
+        collection.find().toArray(function(err, docs){
+          onQueryFinishedCallback(docs);
+      }
+    })
+  }
+
+}
+
+module.exports = dbQueryHelper;
